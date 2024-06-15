fe7faa1496687c723b27c035ceccccc0ca13dd42
alexmathews89 <alexmathews@live.com>
2024-06-14 20:59:58 -0700

    started css

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -10,6 +10,9 @@ const NumberOfEvents = ({ setCurrentNOE }) => {
   };
   return (
     <div id="number-of-events">
+      <label>
+        <h1>Number of Events: </h1>
+      </label>
       <input type="text" value={eventNumber} onChange={handleInputChange} />
     </div>
   );
